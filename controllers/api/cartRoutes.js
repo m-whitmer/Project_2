0ef7f96a60a94ab6d7033bbee359762bf00311b9
Ethan Dutcher <74162204+edutcher@users.noreply.github.com>
2021-03-30 13:55:13 -0400

    update quantity and delete

--- controllers/api/cartRoutes.js
+++ controllers/api/cartRoutes.js
@@ -75,4 +75,74 @@ router.post('/', async(req, res) => {
     }
 })
 
+router.delete('/::id', async(req, res) => {
+    if (!req.session.logged_in) {
+
+        res.status(400).json({ message: "Please log in" })
+        return;
+    }
+
+    let id = parseInt(req.params.id);
+    let fetchedCart;
+    try {
+        fetchedCart = await Cart.findOne({
+            include: [{ model: Product, through: CartItem }, { model: User }],
+            exclude: ['password'],
+            where: {
+                user_id: req.session.user_id
+            }
+        })
+
+        let products = await fetchedCart.getProducts({
+            where: {
+                id
+            }
+        })
+
+        let product = products[0];
+
+        return await product.cartItem.destroy();
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
+
+router.put('/', async(req, res) => {
+    if (!req.session.logged_in) {
+
+        res.status(400).json({ message: "Please log in" })
+        return;
+    }
+
+    let { id, quantity } = req.body;
+    let fetchedCart;
+    try {
+        fetchedCart = await Cart.findOne({
+            include: [{ model: Product, through: CartItem }, { model: User }],
+            exclude: ['password'],
+            where: {
+                user_id: req.session.user_id
+            }
+        })
+
+        let products = await fetchedCart.getProducts({
+            where: {
+                id
+            }
+        })
+
+        let product;
+        if (products.length > 0) {
+            product = products[0]
+        }
+
+        return await fetchedCart.addProduct(product, { through: { quantity } });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
+})
+
 module.exports = router;
--- public/assets/js/cart.js
+++ public/assets/js/cart.js
@@ -1,22 +1,58 @@
 let curTotal;
 
+let renderTotal = () => {
+    let newRow = $('<tr>');
+    let emptyData1 = $('<td>');
+    let emptyData2 = $('<td>');
+    let emptyData3 = $('<td>');
+    let titleData = $('<td>');
+    let totalTitle = $('<h5>').text('Total:');
+    let totalData = $('<td>').addClass('text-right');
+    let totalHeader = $('<h5>').text(curTotal);
+
+    titleData.append(totalTitle);
+
+    totalData.append(totalHeader);
+
+    newRow.append(emptyData1, emptyData2, emptyData3, titleData, totalData);
+
+    $('#prodTable').append(newRow);
+}
+
+let renderButtons = () => {
+    let newRow = $('<tr>');
+    let emptyData1 = $('<td>');
+    let emptyData2 = $('<td>');
+    let emptyData3 = $('<td>');
+    let shopData = $('<td>');
+    let shopBtn = $('<td>').addClass('btn btn-primary').text('Continue Shopping');
+    let orderData = $('<td>');
+    let orderBtn = $('<td>').addClass('btn btn-success').text('Place order');
+
+    shopData.append(shopBtn);
+    orderData.append(orderBtn);
+
+    newRow.append(emptyData1, emptyData2, emptyData3, shopData, orderData);
+
+    $('#prodTable').append(newRow);
+}
+
 let renderProduct = product => {
     let newRow = $('<tr>');
     let dataOne = $('<td>').addClass('col-md-6');
     let newDiv = $('<div>').addClass('media');
     let newAnch = $('<a>').addClass('thumbnail pull-left');
-    let newImg = $('<img>').addClass('media-object').attr('src', product.image_url);
+    let newImg = $('<img>').addClass('media-object').attr('src', product.image_url).css('height', '72px').css('width', '72px');
     let newerDiv = $('<div>').addClass('media-body');
     let newH4 = $('<h4>').addClass('media-heading');
-    let newerAnch = $('<a>').text(product.product_name).attr('href', `/item/${product.id}`);
+    let newerAnch = $('<a>').addClass('ml-2').text(product.product_name).attr('href', `/item/${product.id}`);
     let dataTwo = $('<td>').addClass('col-sm-1 col-md-1');
-    let newInput = $('<input>').addClass('form-control').attr('type', 'number').attr('value', product.cartItem.quantity);
+    let newInput = $('<input>').addClass('form-control').attr('type', 'number').attr('value', product.cartItem.quantity).data('id', product.id);
     let dataThree = $('<td>').addClass('col-sm-1 col-md-1 text-center').text(product.price);
     let dataFour = $('<td>').addClass('col-sm-1 col-md-1 text-center').text(product.cartItem.quantity * product.price);
     let dataFive = $('<td>').addClass('col-sm-1 col-md-1');
-    let newBtn = $('<button>').addClass('btn btn-danger').text('Remove');
-    let newSpan = $('<span>').addClass('glyphicon glyphicon-remove');
-    newBtn.prepend(newSpan);
+    let newBtn = $('<button>').addClass('btn btn-danger delBtn').text('Remove').data('id', product.id);
+
     dataFive.append(newBtn);
 
     dataTwo.append(newInput);
@@ -44,6 +80,8 @@ let showCart = cart => {
     for (let product of cart) {
         renderProduct(product);
     }
+    renderTotal();
+    renderButtons();
 }
 
 let loadCart = () => {
@@ -52,12 +90,44 @@ let loadCart = () => {
             showCart(res.data.products);
         })
         .catch(err => {
-
+            console.log(err);
         })
 }
 
+let delShoe = id => {
+    axios.delete(`/api/carts/:${id}`)
+        .then(res => {})
+        .catch(err => {
+            console.log(err);
+        })
+    setTimeout(loadCart, 300);
+}
+
+
+let updateQuantity = (id, quantity) => {
+    let data = {
+        id,
+        quantity
+    }
+
+    axios.put('/api/carts', data).then(res => {
+        console.log(res);
+        setTimeout(window.location.reload(), 300);
+    }).catch(err => {
+        console.log(err);
+    })
+    setTimeout(window.location.reload(), 300);
+}
 
 $(document).ready(() => {
+
+    $(document).on('click', '.delBtn', function() {
+        delShoe($(this).data('id'));
+    })
+
+    $(document).on('change', 'input', function() {
+        updateQuantity($(this).data('id'), $(this).val());
+    })
 
     loadCart();
 })
